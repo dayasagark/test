8f88d17c199e565c1b377a21d79527d628e91762
dayasagark <dayasagar940@gmail.com>
2020-04-29 15:34:21 +0530

    only 3 hello worlds

--- index.php
+++ index.php
@@ -2,8 +2,4 @@
     echo "Hello World";
     echo "Hello World1";
     echo "Hello World2";
-    echo "Hello World3";
-    echo "Hello World4";
-    echo "Hello World5";
-    echo "Hello World6";
 ?>
